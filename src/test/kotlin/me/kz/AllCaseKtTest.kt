dc276b94db217d4993b50d17863f7fe8700b45a6
x <axpq110@gmail.com>
2022-08-23 16:55:18 +0800

    add case: actorBUFFERED, executorArrayBlockingQueue

--- src/test/kotlin/me/kz/AllCaseKtTest.kt
+++ src/test/kotlin/me/kz/AllCaseKtTest.kt
@@ -2,6 +2,7 @@ package me.kz
 
 import kotlinx.coroutines.channels.Channel
 import org.openjdk.jmh.annotations.*
+import java.util.concurrent.ArrayBlockingQueue
 import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.SynchronousQueue
 
@@ -14,10 +15,16 @@ import java.util.concurrent.SynchronousQueue
 open class AllCaseKtTest {
 
     private val n = 99999
+    private val bufferSize = n / 3
 
     @Benchmark
     fun actorRENDEZVOUS() {
         actor(n, Channel.RENDEZVOUS)
+    }
+
+    @Benchmark
+    fun actorBUFFERED() {
+        actor(n, bufferSize)
     }
 
     @Benchmark
@@ -31,17 +38,25 @@ open class AllCaseKtTest {
     }
 
     @Benchmark
+    fun executorArrayBlockingQueue() {
+        executor(n, ArrayBlockingQueue(bufferSize))
+    }
+
+    @Benchmark
     fun executorLinkedBlockingQueue() {
         executor(n, LinkedBlockingQueue())
     }
 }
 
-// -Xmx2g -Xms2g -Xmn1g
+// -Xmx4g -Xms4g -Xmn2g
 
 // n = 99999
+// bufferSize = n / 3
 
 // Benchmark                                  Mode  Cnt  Score    Error  Units
-// AllCaseKtTest.actorRENDEZVOUS              avgt   25  0.192 ±  0.048   s/op
-// AllCaseKtTest.actorUNLIMITED               avgt   25  0.037 ±  0.002   s/op
+// AllCaseKtTest.actorRENDEZVOUS              avgt   25  0.209 ±  0.048   s/op
+// AllCaseKtTest.actorBUFFERED                avgt   25  0.031 ±  0.001   s/op
+// AllCaseKtTest.actorUNLIMITED               avgt   25  0.035 ±  0.001   s/op
+// AllCaseKtTest.executorSynchronousQueue     avgt   25  0.053 ±  0.001   s/op
+// AllCaseKtTest.executorArrayBlockingQueue   avgt   25  0.016 ±  0.001   s/op
 // AllCaseKtTest.executorLinkedBlockingQueue  avgt   25  0.029 ±  0.001   s/op
-// AllCaseKtTest.executorSynchronousQueue     avgt   25  0.051 ±  0.001   s/op
